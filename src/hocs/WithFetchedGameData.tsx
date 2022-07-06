4c6358eebf9fa48806ca1f1cb3f2a2704b4b30c7
Jani <jani.claren@gmail.com>
2022-07-06 23:14:03 +0200

    POC

--- src/hocs/WithFetchedGameData.tsx
+++ src/hocs/WithFetchedGameData.tsx
@@ -1,11 +1,15 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 
-function Error(props:any) {
+function Error(props: any) {
   return <div className="error">{props.message}</div>;
 }
 
-export function withFetchedGameData(WrappedComponent: any, url: string, errorMessage:string) {
+export function withFetchedGameData(
+  WrappedComponent: any,
+  url: string,
+  errorMessage: string
+) {
   return (props: any) => {
     const [items, setItems] = useState([]);
     const [fetchError, setFetchError] = useState(false);
@@ -14,17 +18,24 @@ export function withFetchedGameData(WrappedComponent: any, url: string, errorMes
       // @ts-ignore
       axios
         .get(url)
-        .then((response: any) => {          
-          if( response.data.body.games.length == 0 || !!response.data.body.games.error ) {
-            setFetchError(true)
-            return
-          }          
+        .then((response: any) => {
+          if (
+            response.data.body.games.length == 0 ||
+            !!response.data.body.games.error
+          ) {
+            setFetchError(true);
+            return;
+          }
 
-          setItems(response.data.body.games);            
+          setItems(response.data.body.games);
         })
         .catch((err: any) => setFetchError(true));
     });
 
-    return fetchError ? <Error message={errorMessage}/> : <WrappedComponent items={items} />;
+    return fetchError ? (
+      <Error message={errorMessage} />
+    ) : (
+      <WrappedComponent items={items} />
+    );
   };
 }
